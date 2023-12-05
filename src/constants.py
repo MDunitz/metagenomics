f06e04879bff604642fb6f1b49e5e97d3a1fcca2
mdunitz <7864111+MDunitz@users.noreply.github.com>
2023-12-04 17:22:17 -0800

    sp

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+
+ANALYSIS_SUMMARY_COL_NAMES = ['Submitted nucleotide sequences',
+ 'Nucleotide sequences after format-specific filtering',
+ 'Nucleotide sequences after length filtering',
+ 'Nucleotide sequences after undetermined bases filtering',
+ 'Reads with predicted CDS',
+ 'Reads with predicted RNA',
+ 'Reads with InterProScan match',
+ 'Predicted CDS',
+ 'Predicted CDS with InterProScan match',
+ 'Total InterProScan matches']
--- src/extract.py
+++ src/extract.py
@@ -2,6 +2,8 @@
 import pandas as pd
 ## todo switch to dask
 from jsonapi_client import Session, Modifier
+from urllib.request import urlretrieve
+from src.constants import ANALYSIS_SUMMARY_COL_NAMES
 
 def fetch_metadata(endpoint, api_path):
     with Session(api_path) as mgnify:
@@ -24,6 +26,10 @@ def fetch_study_data(api_path, study_metadata, sample_count=6):
                 'study': study.id, 
                 'lon': samples['attributes.longitude'],
                 'lat': samples['attributes.latitude'],
+                'sample_description': samples['attributes.sample-desc'],
+                'biome': samples['attributes.environment-biome'],
+                'environment': samples['attributes.environment-feature'],
+                'biome_relationship': samples['relationships.biome.data.id'],
                 'color': "#FF0000",
             })
             samples.set_index('accession', inplace=True)
@@ -35,9 +41,41 @@ def get_sample_analyses(api_path, study_samples, analysis_limit=10):
     analyses = []
     with Session(api_path) as mgnify:
         for idx, sample in study_samples[:analysis_limit].iterrows():
-            filtering = Modifier(f"pipeline_version=5.0&sample_accession={sample.sample_id}&experiment_type=assembly")
+            # filtering = Modifier(f"pipeline_version=5.0&sample_accession={sample.sample_id}&experiment_type=assembly")
+            filtering = Modifier(f"sample_accession={sample.sample_id}")
             analysis = map(lambda r: r.json, mgnify.iterate('analyses', filter=filtering))
             analysis = pd.json_normalize(analysis)
             analyses.append(analysis)
-    analyses = pd.concat(analyses)
-    return analyses
+    analyses_df = pd.concat(analyses)
+    analyses_df[ANALYSIS_SUMMARY_COL_NAMES] = analyses_df.apply(handle_analysis_summary, axis='columns', result_type='expand')
+    return analyses_df
+
+
+def handle_analysis_summary(row):
+    analysis_values = []
+    sample_row_summaries = row['attributes.analysis-summary']
+    for i in range(0,10):
+        analysis_values.append(sample_row_summaries[i]['value'])
+    return analysis_values
+
+def check_available_sample_analysis_data(api_path, analysis_id):
+    with Session(api_path) as session:
+        print(f"\nFiles available for analysis {analysis_id}:")
+        for download in session.iterate(f"analyses/{analysis_id}/downloads"):
+            print(f"{download.alias}: {download.description.label}")
+            print(f"relevant url: {download.links.self.url}")
+
+
+def get_data_file(file_path, download_url):
+    # with Session(api_path) as session:    
+        # print(f"Processing: {analysis_id}")
+        # for download in session.iterate(f"analyses/{analysis_id}/downloads"):
+        #     # Start another for loop to go over the files to download
+        #     file_name = f"../tmp/{analysis_id}_FASTQ_SSU_OTU.tsv"
+        #     print(f"Downloading file for {analysis_id}")
+        #     try:
+    try:
+        urlretrieve(download_url, file_path)
+    except Exception as e:
+        print(e)
+    # print(f"Finished for: {analysis_id}, stored: {file_path}")
